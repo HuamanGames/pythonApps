0d97346d8608b65a9a6055e3d948bed61cfc3fca
SPclub BONG <SPBong999@gmail.com>
2018-04-12 15:32:33 +0200

    Update HuamanDefinitionModule.py

--- py/HuamanDefinitionModule.py
+++ py/HuamanDefinitionModule.py
@@ -175,173 +175,188 @@ def Favsuperhero():
 
 
 
-def CrazyLights():
-    user32 = ctypes.WinDLL('user32', use_last_error=True)
 
-    INPUT_MOUSE    = 0
-    INPUT_KEYBOARD = 1
-    INPUT_HARDWARE = 2
+user32 = ctypes.WinDLL('user32', use_last_error=True)
 
-    KEYEVENTF_EXTENDEDKEY = 0x0001
-    KEYEVENTF_KEYUP       = 0x0002
-    KEYEVENTF_UNICODE     = 0x0004
-    KEYEVENTF_SCANCODE    = 0x0008
+INPUT_MOUSE    = 0
+INPUT_KEYBOARD = 1
+INPUT_HARDWARE = 2
 
-    MAPVK_VK_TO_VSC = 0
-    #Here are all the VK and WH virtual key codes.
-    WH_MIN = -1
-    WH_MSGFILTER = -1
-    WH_JOURNALRECORD = 0
-    WH_JOURNALPLAYBACK = 1
-    WH_KEYBOARD = 2
-    WH_GETMESSAGE = 3
-    WH_CALLWNDPROC = 4
-    WH_CBT = 5
-    WH_SYSMSGFILTER = 6
-    WH_MOUSE = 7
-    WH_HARDWARE = 8
-    WH_DEBUG = 9
-    WH_SHELL = 10
-    WH_FOREGROUNDIDLE = 11
-    WH_CALLWNDPROCRET = 12
-    WH_KEYBOARD_LL = 13
-    WH_MOUSE_LL = 14
-    WH_MAX = 15
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP       = 0x0002
+KEYEVENTF_UNICODE     = 0x0004
+KEYEVENTF_SCANCODE    = 0x0008
 
-    WM_MOUSEFIRST = 0x0200
-    WM_MOUSEMOVE = 0x0200
-    WM_LBUTTONDOWN = 0x0201
-    WM_LBUTTONUP = 0x0202
-    WM_LBUTTONDBLCLK = 0x0203
-    WM_RBUTTONDOWN =0x0204
-    WM_RBUTTONUP = 0x0205
-    WM_RBUTTONDBLCLK = 0x0206
-    WM_MBUTTONDOWN = 0x0207
-    WM_MBUTTONUP = 0x0208
-    WM_MBUTTONDBLCLK = 0x0209
-    WM_MOUSEWHEEL = 0x020A
-    WM_MOUSELAST = 0x020A
+MAPVK_VK_TO_VSC = 0
+#Here are all the VK and WH virtual key codes.
+WH_MIN = -1
+WH_MSGFILTER = -1
+WH_JOURNALRECORD = 0
+WH_JOURNALPLAYBACK = 1
+WH_KEYBOARD = 2
+WH_GETMESSAGE = 3
+WH_CALLWNDPROC = 4
+WH_CBT = 5
+WH_SYSMSGFILTER = 6
+WH_MOUSE = 7
+WH_HARDWARE = 8
+WH_DEBUG = 9
+WH_SHELL = 10
+WH_FOREGROUNDIDLE = 11
+WH_CALLWNDPROCRET = 12
+WH_KEYBOARD_LL = 13
+WH_MOUSE_LL = 14
+WH_MAX = 15
 
-    WM_KEYFIRST = 0x0100
-    WM_KEYDOWN = 0x0100
-    WM_KEYUP = 0x0101
-    WM_CHAR = 0x0102
-    WM_DEADCHAR = 0x0103
-    WM_SYSKEYDOWN = 0x0104
-    WM_SYSKEYUP = 0x0105
-    WM_SYSCHAR = 0x0106
-    WM_SYSDEADCHAR = 0x0107
-    WM_KEYLAST = 0x0108
+WM_MOUSEFIRST = 0x0200
+WM_MOUSEMOVE = 0x0200
+WM_LBUTTONDOWN = 0x0201
+WM_LBUTTONUP = 0x0202
+WM_LBUTTONDBLCLK = 0x0203
+WM_RBUTTONDOWN =0x0204
+WM_RBUTTONUP = 0x0205
+WM_RBUTTONDBLCLK = 0x0206
+WM_MBUTTONDOWN = 0x0207
+WM_MBUTTONUP = 0x0208
+WM_MBUTTONDBLCLK = 0x0209
+WM_MOUSEWHEEL = 0x020A
+WM_MOUSELAST = 0x020A
+
+WM_KEYFIRST = 0x0100
+WM_KEYDOWN = 0x0100
+WM_KEYUP = 0x0101
+WM_CHAR = 0x0102
+WM_DEADCHAR = 0x0103
+WM_SYSKEYDOWN = 0x0104
+WM_SYSKEYUP = 0x0105
+WM_SYSCHAR = 0x0106
+WM_SYSDEADCHAR = 0x0107
+WM_KEYLAST = 0x0108
 
 
-    #VK_0 thru VK_9 are the same as ASCII '0' thru '9' (0x30 -' : 0x39)
-    #VK_A thru VK_Z are the same as ASCII 'A' thru 'Z' (0x41 -' : 0x5A)
+#VK_0 thru VK_9 are the same as ASCII '0' thru '9' (0x30 -' : 0x39)
+#VK_A thru VK_Z are the same as ASCII 'A' thru 'Z' (0x41 -' : 0x5A)
 
-    #virtual keycode constant names to virtual keycodes numerical id
-    vk_to_id = {'VK_LBUTTON' : 0x01, 'VK_RBUTTON' : 0x02, 'VK_CANCEL' : 0x03, 'VK_MBUTTON' : 0x04,
-    'VK_BACK' : 0x08, 'VK_TAB' : 0x09, 'VK_CLEAR' : 0x0C, 'VK_RETURN' : 0x0D, 'VK_SHIFT' : 0x10,
-    'VK_CONTROL' : 0x11, 'VK_MENU' : 0x12, 'VK_PAUSE' : 0x13, 'VK_CAPITAL' : 0x14, 'VK_KANA' : 0x15,
-    'VK_HANGEUL' : 0x15, 'VK_HANGUL' : 0x15, 'VK_JUNJA' : 0x17, 'VK_FINAL' : 0x18, 'VK_HANJA' : 0x19,
-    'VK_KANJI' : 0x19, 'VK_ESCAPE' : 0x1B, 'VK_CONVERT' : 0x1C, 'VK_NONCONVERT' : 0x1D, 'VK_ACCEPT' : 0x1E,
-    'VK_MODECHANGE' : 0x1F, 'VK_SPACE' : 0x20, 'VK_PRIOR' : 0x21, 'VK_NEXT' : 0x22, 'VK_END' : 0x23,
-    'VK_HOME' : 0x24, 'VK_LEFT' : 0x25, 'VK_UP' : 0x26, 'VK_RIGHT' : 0x27, 'VK_DOWN' : 0x28,
-    'VK_SELECT' : 0x29, 'VK_PRINT' : 0x2A, 'VK_EXECUTE' : 0x2B, 'VK_SNAPSHOT' : 0x2C, 'VK_INSERT' : 0x2D,
-    'VK_DELETE' : 0x2E, 'VK_HELP' : 0x2F, 'VK_LWIN' : 0x5B, 'VK_RWIN' : 0x5C, 'VK_APPS' : 0x5D,
-    'VK_NUMPAD0' : 0x60, 'VK_NUMPAD1' : 0x61, 'VK_NUMPAD2' : 0x62, 'VK_NUMPAD3' : 0x63, 'VK_NUMPAD4' : 0x64,
-    'VK_NUMPAD5' : 0x65, 'VK_NUMPAD6' : 0x66, 'VK_NUMPAD7' : 0x67, 'VK_NUMPAD8' : 0x68, 'VK_NUMPAD9' : 0x69,
-    'VK_MULTIPLY' : 0x6A, 'VK_ADD' : 0x6B, 'VK_SEPARATOR' : 0x6C, 'VK_SUBTRACT' : 0x6D, 'VK_DECIMAL' : 0x6E,
-    'VK_DIVIDE' : 0x6F ,'VK_F1' : 0x70, 'VK_F2' : 0x71, 'VK_F3' : 0x72, 'VK_F4' : 0x73, 'VK_F5' : 0x74,
-    'VK_F6' : 0x75, 'VK_F7' : 0x76, 'VK_F8' : 0x77, 'VK_F9' : 0x78, 'VK_F10' : 0x79, 'VK_F11' : 0x7A,
-    'VK_F12' : 0x7B, 'VK_F13' : 0x7C, 'VK_F14' : 0x7D, 'VK_F15' : 0x7E, 'VK_F16' : 0x7F, 'VK_F17' : 0x80,
-    'VK_F18' : 0x81, 'VK_F19' : 0x82, 'VK_F20' : 0x83, 'VK_F21' : 0x84, 'VK_F22' : 0x85, 'VK_F23' : 0x86,
-    'VK_F24' : 0x87, 'VK_NUMLOCK' : 0x90, 'VK_SCROLL' : 0x91, 'VK_LSHIFT' : 0xA0, 'VK_RSHIFT' : 0xA1,
-    'VK_LCONTROL' : 0xA2, 'VK_RCONTROL' : 0xA3, 'VK_LMENU' : 0xA4, 'VK_RMENU' : 0xA5, 'VK_PROCESSKEY' : 0xE5,
-    'VK_ATTN' : 0xF6, 'VK_CRSEL' : 0xF7, 'VK_EXSEL' : 0xF8, 'VK_EREOF' : 0xF9, 'VK_PLAY' : 0xFA,
-    'VK_ZOOM' : 0xFB, 'VK_NONAME' : 0xFC, 'VK_PA1' : 0xFD, 'VK_OEM_CLEAR' : 0xFE, 'VK_BROWSER_BACK' : 0xA6,
-    'VK_BROWSER_FORWARD' : 0xA7, 'VK_BROWSER_REFRESH' : 0xA8, 'VK_BROWSER_STOP' : 0xA9, 'VK_BROWSER_SEARCH' : 0xAA,
-    'VK_BROWSER_FAVORITES' : 0xAB, 'VK_BROWSER_HOME' : 0xAC, 'VK_VOLUME_MUTE' : 0xAD, 'VK_VOLUME_DOWN' : 0xAE,
-    'VK_VOLUME_UP' : 0xAF, 'VK_MEDIA_NEXT_TRACK' : 0xB0, 'VK_MEDIA_PREV_TRACK' : 0xB1, 'VK_MEDIA_STOP' : 0xB2,
-    'VK_MEDIA_PLAY_PAUSE' : 0xB3, 'VK_LAUNCH_MAIL' : 0xB4, 'VK_LAUNCH_MEDIA_SELECT' : 0xB5, 'VK_LAUNCH_APP1' : 0xB6,
-    'VK_LAUNCH_APP2' : 0xB7, 'VK_OEM_1' : 0xBA, 'VK_OEM_PLUS' : 0xBB, 'VK_OEM_COMMA' : 0xBC, 'VK_OEM_MINUS' : 0xBD,
-    'VK_OEM_PERIOD' : 0xBE, 'VK_OEM_2' : 0xBF, 'VK_OEM_3' : 0xC0, 'VK_OEM_4' : 0xDB, 'VK_OEM_5' : 0xDC,
-    'VK_OEM_6' : 0xDD, 'VK_OEM_7' : 0xDE, 'VK_OEM_8' : 0xDF, 'VK_OEM_102' : 0xE2, 'VK_PROCESSKEY' : 0xE5,
-    'VK_PACKET' : 0xE7}
+#virtual keycode constant names to virtual keycodes numerical id
+vk_to_id = {'VK_LBUTTON' : 0x01, 'VK_RBUTTON' : 0x02, 'VK_CANCEL' : 0x03, 'VK_MBUTTON' : 0x04,
+'VK_BACK' : 0x08, 'VK_TAB' : 0x09, 'VK_CLEAR' : 0x0C, 'VK_RETURN' : 0x0D, 'VK_SHIFT' : 0x10,
+'VK_CONTROL' : 0x11, 'VK_MENU' : 0x12, 'VK_PAUSE' : 0x13, 'VK_CAPITAL' : 0x14, 'VK_KANA' : 0x15,
+'VK_HANGEUL' : 0x15, 'VK_HANGUL' : 0x15, 'VK_JUNJA' : 0x17, 'VK_FINAL' : 0x18, 'VK_HANJA' : 0x19,
+'VK_KANJI' : 0x19, 'VK_ESCAPE' : 0x1B, 'VK_CONVERT' : 0x1C, 'VK_NONCONVERT' : 0x1D, 'VK_ACCEPT' : 0x1E,
+'VK_MODECHANGE' : 0x1F, 'VK_SPACE' : 0x20, 'VK_PRIOR' : 0x21, 'VK_NEXT' : 0x22, 'VK_END' : 0x23,
+'VK_HOME' : 0x24, 'VK_LEFT' : 0x25, 'VK_UP' : 0x26, 'VK_RIGHT' : 0x27, 'VK_DOWN' : 0x28,
+'VK_SELECT' : 0x29, 'VK_PRINT' : 0x2A, 'VK_EXECUTE' : 0x2B, 'VK_SNAPSHOT' : 0x2C, 'VK_INSERT' : 0x2D,
+'VK_DELETE' : 0x2E, 'VK_HELP' : 0x2F, 'VK_LWIN' : 0x5B, 'VK_RWIN' : 0x5C, 'VK_APPS' : 0x5D,
+'VK_NUMPAD0' : 0x60, 'VK_NUMPAD1' : 0x61, 'VK_NUMPAD2' : 0x62, 'VK_NUMPAD3' : 0x63, 'VK_NUMPAD4' : 0x64,
+'VK_NUMPAD5' : 0x65, 'VK_NUMPAD6' : 0x66, 'VK_NUMPAD7' : 0x67, 'VK_NUMPAD8' : 0x68, 'VK_NUMPAD9' : 0x69,
+'VK_MULTIPLY' : 0x6A, 'VK_ADD' : 0x6B, 'VK_SEPARATOR' : 0x6C, 'VK_SUBTRACT' : 0x6D, 'VK_DECIMAL' : 0x6E,
+'VK_DIVIDE' : 0x6F ,'VK_F1' : 0x70, 'VK_F2' : 0x71, 'VK_F3' : 0x72, 'VK_F4' : 0x73, 'VK_F5' : 0x74,
+'VK_F6' : 0x75, 'VK_F7' : 0x76, 'VK_F8' : 0x77, 'VK_F9' : 0x78, 'VK_F10' : 0x79, 'VK_F11' : 0x7A,
+'VK_F12' : 0x7B, 'VK_F13' : 0x7C, 'VK_F14' : 0x7D, 'VK_F15' : 0x7E, 'VK_F16' : 0x7F, 'VK_F17' : 0x80,
+'VK_F18' : 0x81, 'VK_F19' : 0x82, 'VK_F20' : 0x83, 'VK_F21' : 0x84, 'VK_F22' : 0x85, 'VK_F23' : 0x86,
+'VK_F24' : 0x87, 'VK_NUMLOCK' : 0x90, 'VK_SCROLL' : 0x91, 'VK_LSHIFT' : 0xA0, 'VK_RSHIFT' : 0xA1,
+'VK_LCONTROL' : 0xA2, 'VK_RCONTROL' : 0xA3, 'VK_LMENU' : 0xA4, 'VK_RMENU' : 0xA5, 'VK_PROCESSKEY' : 0xE5,
+'VK_ATTN' : 0xF6, 'VK_CRSEL' : 0xF7, 'VK_EXSEL' : 0xF8, 'VK_EREOF' : 0xF9, 'VK_PLAY' : 0xFA,
+'VK_ZOOM' : 0xFB, 'VK_NONAME' : 0xFC, 'VK_PA1' : 0xFD, 'VK_OEM_CLEAR' : 0xFE, 'VK_BROWSER_BACK' : 0xA6,
+'VK_BROWSER_FORWARD' : 0xA7, 'VK_BROWSER_REFRESH' : 0xA8, 'VK_BROWSER_STOP' : 0xA9, 'VK_BROWSER_SEARCH' : 0xAA,
+'VK_BROWSER_FAVORITES' : 0xAB, 'VK_BROWSER_HOME' : 0xAC, 'VK_VOLUME_MUTE' : 0xAD, 'VK_VOLUME_DOWN' : 0xAE,
+'VK_VOLUME_UP' : 0xAF, 'VK_MEDIA_NEXT_TRACK' : 0xB0, 'VK_MEDIA_PREV_TRACK' : 0xB1, 'VK_MEDIA_STOP' : 0xB2,
+'VK_MEDIA_PLAY_PAUSE' : 0xB3, 'VK_LAUNCH_MAIL' : 0xB4, 'VK_LAUNCH_MEDIA_SELECT' : 0xB5, 'VK_LAUNCH_APP1' : 0xB6,
+'VK_LAUNCH_APP2' : 0xB7, 'VK_OEM_1' : 0xBA, 'VK_OEM_PLUS' : 0xBB, 'VK_OEM_COMMA' : 0xBC, 'VK_OEM_MINUS' : 0xBD,
+'VK_OEM_PERIOD' : 0xBE, 'VK_OEM_2' : 0xBF, 'VK_OEM_3' : 0xC0, 'VK_OEM_4' : 0xDB, 'VK_OEM_5' : 0xDC,
+'VK_OEM_6' : 0xDD, 'VK_OEM_7' : 0xDE, 'VK_OEM_8' : 0xDF, 'VK_OEM_102' : 0xE2, 'VK_PROCESSKEY' : 0xE5,
+'VK_PACKET' : 0xE7, 'VK_KEY_A' : 0x41,'VK_KEY_B' : 0x42,'VK_KEY_C' : 0x43,'VK_KEY_D' : 0x44,'VK_KEY_E' : 0x45,
+'VK_KEY_F' : 0x46,'VK_KEY_G' : 0x47,'VK_KEY_H' : 0x48,'VK_KEY_I' : 0x49,'VK_KEY_J' : 0x4A,'VK_KEY_K' : 0x4B,
+'VK_KEY_L' : 0x4C,'VK_KEY_M' : 0x4D,'VK_KEY_N' : 0x4E,'VK_KEY_O' : 0x4F,'VK_KEY_P' : 0x50,'VK_KEY_Q' : 0x51,
+'VK_KEY_R' : 0x52,'VK_KEY_S' : 0x53,'VK_KEY_T' : 0x54,'VK_KEY_U' : 0x55,'VK_KEY_V' : 0x56,'VK_KEY_W' : 0x57,
+'VK_KEY_X' : 0x58,'VK_KEY_Y' : 0x59,'VK_KEY_Z' : 0x5A}
 
-    #inverse mapping of keycodes
-    id_to_vk = dict([(v,k) for k,v in vk_to_id.items()])
+#inverse mapping of keycodes
+id_to_vk = dict([(v,k) for k,v in vk_to_id.items()])
 
-    #message constants to message names
-    msg_to_name = {WM_MOUSEMOVE : 'mouse move', WM_LBUTTONDOWN : 'mouse left down',
-                 WM_LBUTTONUP : 'mouse left up', WM_LBUTTONDBLCLK : 'mouse left double',
-                 WM_RBUTTONDOWN : 'mouse right down', WM_RBUTTONUP : 'mouse right up',
-                 WM_RBUTTONDBLCLK : 'mouse right double',  WM_MBUTTONDOWN : 'mouse middle down',
-                 WM_MBUTTONUP : 'mouse middle up', WM_MBUTTONDBLCLK : 'mouse middle double',
-                 WM_MOUSEWHEEL : 'mouse wheel',  WM_KEYDOWN : 'key down',
-                 WM_KEYUP : 'key up', WM_CHAR : 'key char', WM_DEADCHAR : 'key dead char',
-                 WM_SYSKEYDOWN : 'key sys down', WM_SYSKEYUP : 'key sys up',
-                 WM_SYSCHAR : 'key sys char', WM_SYSDEADCHAR : 'key sys dead char'}
+#message constants to message names
+msg_to_name = {WM_MOUSEMOVE : 'mouse move', WM_LBUTTONDOWN : 'mouse left down',
+             WM_LBUTTONUP : 'mouse left up', WM_LBUTTONDBLCLK : 'mouse left double',
+             WM_RBUTTONDOWN : 'mouse right down', WM_RBUTTONUP : 'mouse right up',
+             WM_RBUTTONDBLCLK : 'mouse right double',  WM_MBUTTONDOWN : 'mouse middle down',
+             WM_MBUTTONUP : 'mouse middle up', WM_MBUTTONDBLCLK : 'mouse middle double',
+             WM_MOUSEWHEEL : 'mouse wheel',  WM_KEYDOWN : 'key down',
+             WM_KEYUP : 'key up', WM_CHAR : 'key char', WM_DEADCHAR : 'key dead char',
+             WM_SYSKEYDOWN : 'key sys down', WM_SYSKEYUP : 'key sys up',
+             WM_SYSCHAR : 'key sys char', WM_SYSDEADCHAR : 'key sys dead char'}
+
+wintypes.ULONG_PTR = wintypes.WPARAM
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = (("dx",          wintypes.LONG),
+                ("dy",          wintypes.LONG),
+                ("mouseData",   wintypes.DWORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = (("wVk",         wintypes.WORD),
+                ("wScan",       wintypes.WORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+    def __init__(self, *args, **kwds):
+        super(KEYBDINPUT, self).__init__(*args, **kwds)
+        if not self.dwFlags & KEYEVENTF_UNICODE:
+            self.wScan = user32.MapVirtualKeyExW(self.wVk,
+                                                 MAPVK_VK_TO_VSC, 0)
+
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = (("uMsg",    wintypes.DWORD),
+                ("wParamL", wintypes.WORD),
+                ("wParamH", wintypes.WORD))
+
+class INPUT(ctypes.Structure):
+    class _INPUT(ctypes.Union):
+        _fields_ = (("ki", KEYBDINPUT),
+                    ("mi", MOUSEINPUT),
+                    ("hi", HARDWAREINPUT))
+    _anonymous_ = ("_input",)
+    _fields_ = (("type",   wintypes.DWORD),
+                ("_input", _INPUT))
+
+LPINPUT = ctypes.POINTER(INPUT)
+
+def _check_count(result, func, args):
+    if result == 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+    return args
+
+user32.SendInput.errcheck = _check_count
+user32.SendInput.argtypes = (wintypes.UINT, # nInputs
+                             LPINPUT,       # pInputs
+                             ctypes.c_int)  # cbSize
+
+# Functions
+
+def PressKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode,
+                            dwFlags=KEYEVENTF_KEYUP))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
     
-    wintypes.ULONG_PTR = wintypes.WPARAM
-
-    class MOUSEINPUT(ctypes.Structure):
-        _fields_ = (("dx",          wintypes.LONG),
-                    ("dy",          wintypes.LONG),
-                    ("mouseData",   wintypes.DWORD),
-                    ("dwFlags",     wintypes.DWORD),
-                    ("time",        wintypes.DWORD),
-                    ("dwExtraInfo", wintypes.ULONG_PTR))
-
-    class KEYBDINPUT(ctypes.Structure):
-        _fields_ = (("wVk",         wintypes.WORD),
-                    ("wScan",       wintypes.WORD),
-                    ("dwFlags",     wintypes.DWORD),
-                    ("time",        wintypes.DWORD),
-                    ("dwExtraInfo", wintypes.ULONG_PTR))
-
-        def __init__(self, *args, **kwds):
-            super(KEYBDINPUT, self).__init__(*args, **kwds)
-            if not self.dwFlags & KEYEVENTF_UNICODE:
-                self.wScan = user32.MapVirtualKeyExW(self.wVk,
-                                                     MAPVK_VK_TO_VSC, 0)
-
-    class HARDWAREINPUT(ctypes.Structure):
-        _fields_ = (("uMsg",    wintypes.DWORD),
-                    ("wParamL", wintypes.WORD),
-                    ("wParamH", wintypes.WORD))
-
-    class INPUT(ctypes.Structure):
-        class _INPUT(ctypes.Union):
-            _fields_ = (("ki", KEYBDINPUT),
-                        ("mi", MOUSEINPUT),
-                        ("hi", HARDWAREINPUT))
-        _anonymous_ = ("_input",)
-        _fields_ = (("type",   wintypes.DWORD),
-                    ("_input", _INPUT))
-
-    LPINPUT = ctypes.POINTER(INPUT)
-
-    def _check_count(result, func, args):
-        if result == 0:
-            raise ctypes.WinError(ctypes.get_last_error())
-        return args
-
-    user32.SendInput.errcheck = _check_count
-    user32.SendInput.argtypes = (wintypes.UINT, # nInputs
-                                 LPINPUT,       # pInputs
-                                 ctypes.c_int)  # cbSize
-
-    # Functions
-
-    def PressKey(hexKeyCode):
-        x = INPUT(type=INPUT_KEYBOARD,
-                  ki=KEYBDINPUT(wVk=hexKeyCode))
-        user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
-
-    def ReleaseKey(hexKeyCode):
-        x = INPUT(type=INPUT_KEYBOARD,
-                  ki=KEYBDINPUT(wVk=hexKeyCode,
-                                dwFlags=KEYEVENTF_KEYUP))
-        user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+    
+    
+def EndlessOpenWin():
+    while True:
+        PressKey(0xA2)
+        time.sleep(0.1)
+        PressKey(0x4E)
+        time.sleep(0.1)
+        
+def CrazyLights():
     while True:
         PressKey(0x90)
         time.sleep(0.1)
@@ -355,36 +370,31 @@ def CrazyLights():
         time.sleep(0.1)
         ReleaseKey(0x91)
         time.sleep(0.1)
-        
-    def EndENTERS():
-        time.sleep(2)
-        while True:
-            PressKey(VK_RETURN)
-            time.sleep(0.1)
-            ReleaseKey(VK_RETURN)
-            time.sleep(0.1)
-    def EndKEY_0101():
-        time.sleep(2)
-        while True:
-            PressKey(VK_KEY_0)
-            time.sleep(0.1)
-            PressKey(VK_KEY_1)
-            time.sleep(0.1)
-    def EndKEY_0():
-        time.sleep(2)
-        while True:
-            PressKey(VK_KEY_0)
-            time.sleep(0.1)
-            PressKey(VK_KEY_0)
-            time.sleep(0.1)
-    def EndKEY_1():
-        print("What do you want to Endless?")
-        time.sleep(2)
-        while True:
-            PressKey(VK_KEY_1)
-            time.sleep(0.1)
-            PressKey(VK_KEY_1)
-            time.sleep(0.1)
+
+def EndENTERS():
+    while True:
+        PressKey(VK_RETURN)
+        time.sleep(0.1)
+        ReleaseKey(VK_RETURN)
+        time.sleep(0.1)
+def EndKEY_0101():
+    while True:
+        PressKey(VK_KEY_0)
+        time.sleep(0.1)
+        PressKey(VK_KEY_1)
+        time.sleep(0.1)
+def EndKEY_0():
+    while True:
+        PressKey(VK_KEY_0)
+        time.sleep(0.1)
+        PressKey(VK_KEY_0)
+        time.sleep(0.1)
+def EndKEY_1():
+    while True:
+        PressKey(VK_KEY_1)
+        time.sleep(0.1)
+        PressKey(VK_KEY_1)
+        time.sleep(0.1)
 
 
 
